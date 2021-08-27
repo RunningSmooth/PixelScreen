c0d15d62211e81973acc9a251930b4c200606544
RunningSmooth <to.pitsch97@gmail.com>
2021-08-27 16:09:50 +0200

    changed variable naming and deletet redundant code

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import numpy, pyautogui
 import matplotlib.pyplot as plt
 from matplotlib import animation
-from PIL import Image
 import time
 
 screen_size = (1920, 1080)
-square_num_x = 9
-square_num_y = 16
-len_x = int(1080 / square_num_x)
-len_y = int(1920 / square_num_y)
+
+# common divisors for screen-size (1980, 1080): 
+# 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30, 40, 60, 120
+square_side_length = 8
+square_num_x = int(1080 / square_side_length)
+square_num_y = int(1920 / square_side_length)
 
 numpy_frame = pyautogui.screenshot()
 fig = plt.figure()
@@ -23,13 +24,13 @@ def animate(i):
     part_array = numpy.empty([square_num_x, square_num_y, 3])
     for i in range(square_num_x):
         for j in range(square_num_y):
-            temp = numpy.round(numpy.mean(numpy_frame[i*len_x:(i+1)*len_x, j*len_y:(j+1)*len_y], axis=(0, 1)))
+            temp = numpy.round(numpy.mean(numpy_frame[i*square_side_length:(i+1)*square_side_length, j*square_side_length:(j+1)*square_side_length], axis=(0, 1)))
             part_array[i, j] = temp
     
     # Insert mean
     for r in range(square_num_x):
         for k in range(square_num_y):
-            numpy_frame[r*len_x:(r+1)*len_x, k*len_y:(k+1)*len_y] = part_array[r, k]
+            numpy_frame[r*square_side_length:(r+1)*square_side_length, k*square_side_length:(k+1)*square_side_length] = part_array[r, k]
 
     plt.clf()
     plt.imshow(numpy_frame)
@@ -37,6 +38,6 @@ def animate(i):
     print("Time: " ,end - start, "Seconds")
 
 anim = animation.FuncAnimation(fig, animate,
-                               interval=2000)
+                               frames=60)
 
 plt.show()
