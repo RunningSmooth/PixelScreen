f3e7b31736942e91404d3199cb0f428627f90252
RunningSmooth <to.pitsch97@gmail.com>
2021-08-27 17:25:40 +0200

    deleted redundand for-loops

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # PixelScreen
+
+![Pixel Art Beispiel](pixel_art_demo.png)
+
+## Used technologies / Frameworks
+
+I use the following technologies / frameworks in my project:
+
+- Python
+
+I had the idea to create a program that takes a screenshot and turns the taken picture into a pixel-image. Therefor the 
+
+## Used 3rd Party Libraries
+
+I use the following 3rd party libraries in my project:
+
+Name | Reason
+--- | ---
+[Matplotlib](https://matplotlib.org/) | For displaying (plotting) the image.
+[NumPy](https://numpy.org/) | For downloading the image that was created with 'dom-to-image'
+[FontAwesome](https://fontawesome.com/) | For icons
+
+## Installation / Run
+
+The following components must be installed locally:
+
+- [nodejs](https://nodejs.org/en/) v14.16.0
+- [npm](https://nodejs.org/en/) v6.14.11
+- [Vue.js](https://vuejs.org/) v3.2.4
+
+To run the project locally, enter the following in the command line / bash:
+
+```console
+$ git clone https://github.com/RunningSmooth/PixelArt.git
+$ cd PixelArt
+$ cd client
+$ npm install
+$ npm run serve
+```
+---
--- app.py
+++ app.py
@@ -8,7 +8,7 @@ screen_size = (1920, 1080)
 
 # common divisors for screen-size (1980, 1080): 
 # 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30, 40, 60, 120   -> values for square_side_length
-square_side_length = 12
+square_side_length = 40
 
 # Calculating how many squares are needed in each axial direction.
 square_num_x = int(1920 / square_side_length)
@@ -31,17 +31,11 @@ def animate(i):
     img = pyautogui.screenshot()
     numpy_frame = numpy.asarray(img)
 
-    # Takes squares out of the picture. Calculates and saves the mean-values of their colors in an 3d-Array.
-    part_array = numpy.empty([square_num_y, square_num_x, 3])
+    # Takes squares out of the picture. Calculates and saves the mean-values of their colors in the image array.
     for i in range(square_num_y):
         for j in range(square_num_x):
             temp = numpy.round(numpy.mean(numpy_frame[i*square_side_length:(i+1)*square_side_length, j*square_side_length:(j+1)*square_side_length], axis=(0, 1)))
-            part_array[i, j] = temp
-    
-    # Insert mean-values from the before created 3d Array into the picture array.
-    for r in range(square_num_y):
-        for k in range(square_num_x):
-            numpy_frame[r*square_side_length:(r+1)*square_side_length, k*square_side_length:(k+1)*square_side_length] = part_array[r, k]
+            numpy_frame[i*square_side_length:(i+1)*square_side_length, j*square_side_length:(j+1)*square_side_length] = temp
 
     # Clears plot and plots the image with the mean values.
     plt.clf()
@@ -52,8 +46,7 @@ def animate(i):
     print("Time: " ,end - start, "Seconds")
 
 # Setting an animation for the plot.
-anim = animation.FuncAnimation(fig, animate,
-                               frames=60)
+anim = animation.FuncAnimation(fig, animate, frames=60)
 
 # Show plot.
 plt.show()
