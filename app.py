b0578b58dedd95f1285b8c07767853ae60312818
RunningSmooth <to.pitsch97@gmail.com>
2021-08-27 17:03:19 +0200

    pyplot settings

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ screen_size = (1920, 1080)
 
 # common divisors for screen-size (1980, 1080): 
 # 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30, 40, 60, 120   -> values for square_side_length
-square_side_length = 8
+square_side_length = 12
 
 # Calculating how many squares are needed in each axial direction.
 square_num_x = int(1920 / square_side_length)
@@ -16,8 +16,11 @@ square_num_y = int(1080 / square_side_length)
 
 # Taking first screenshot and adding it to plot.
 numpy_frame = pyautogui.screenshot()
-fig = plt.figure()
+plt.rcParams['toolbar'] = 'None'
+fig = plt.figure(figsize=(12, 7))
+fig.canvas.manager.set_window_title('PixelScreen by RunningSmooth')
 im = plt.imshow(numpy_frame)
+plt.axis('off')
 
 def animate(i):
     """ Function that takes a screenshot and translates it into an pixel-image. """
@@ -43,6 +46,7 @@ def animate(i):
     # Clears plot and plots the image with the mean values.
     plt.clf()
     plt.imshow(numpy_frame)
+    plt.axis('off')
 
     end = time.perf_counter()
     print("Time: " ,end - start, "Seconds")
